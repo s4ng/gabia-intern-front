8cb7e3ac779bbc7b98a3b33400a56a47fe8fad7b
gm2202969 <lsg@gabia.com>
2021-02-05 15:41:17 +0900

    remove a file

--- tests/unit/UserProfile.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import UserProfile from '@/views/pages/UserProfile.vue'
-
-const testString = "this is test"
-
-test('App should work', () => {
-  //const wrapper = shallowMount(UserProfile);
-  expect(testString).toMatch('this is test')
-})
